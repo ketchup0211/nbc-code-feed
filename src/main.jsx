502f6549de5dd1f213a85e59a8ddc2952bbfa7da
ketchup0211 <dlcka1111@naver.com>
2024-02-07 15:53:07 +0900

    [FIX] main.jsx -> apply GlobalStyle

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import GlobalStyle from "GlobalStyle.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
+    <App />
+    <GlobalStyle />
+  </>
+);
