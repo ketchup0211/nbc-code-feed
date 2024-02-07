a5cf0b4d777057cf3d8bc4aa405a596c57fb97ba
ketchup0211 <dlcka1111@naver.com>
2024-02-07 16:59:58 +0900

    [MOD] Router.jsx -> routing SignUp.jsx

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,14 +1,9 @@
 import { useState, useSelector, useDispatch, useContext } from "react";
 import styled from "styled-components";
 
-const title = styled.p`
-  color: black;
-  font-size: 32px;
-`;
-
 function SignUp() {
   console.log("WHYYYYYY");
-  return <title>hi</title>;
+  return <p>hi</p>;
 }
 
 export default SignUp;
--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -6,7 +6,7 @@ const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<p>hello everyone</p>} />
+        <Route path="/" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   );
