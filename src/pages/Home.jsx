6bd72cf73e7e50874325da6102e7c0ca5a44649c
DecidedCard <heajun35@gmail.com>
2024-02-08 15:29:32 +0900

    test: store 저장 테스트

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,52 @@
 import { onAuthStateChanged } from "firebase/auth";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import FilterCheck from "src/components/HomeComponents/FilterCheck";
 import HomeHeader from "src/components/HomeComponents/HomeHeader";
 import PostList from "src/components/HomeComponents/PostList";
-import { auth } from "src/firebase";
+import { auth, db } from "src/firebase";
 import { log } from "src/redux/modules/user";
+import { addTodos } from "src/redux/modules/postList";
+import { addDoc, collection } from "firebase/firestore";
 
 function Home() {
+  const { post } = useSelector((state) => state.postList);
+  const [content, setContent] = useState("");
   const dispatch = useDispatch();
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       dispatch(log(user));
-      console.log(user);
     });
-  });
+  }, [dispatch]);
+
+  const onchangeHandler = () => {
+    setContent(event.target.value);
+  };
+
+  const addTodo = async (event) => {
+    event.preventDefault();
+    const newTodo = { text: content, isDone: false };
+    dispatch(addTodos([{ ...newTodo, id: crypto.randomUUID() }, ...post]));
+    setContent("");
+    const collectionRef = collection(db, "todos");
+    await addDoc(collectionRef, newTodo);
+  };
+
   return (
     <>
       <HomeHeader />
       <FilterCheck />
       <PostList />
+      <form onSubmit={addTodo}>
+        <input
+          type="text"
+          placeholder="내용을 입력해주세요"
+          value={content}
+          onChange={onchangeHandler}
+        />
+        <button>등록</button>
+      </form>
     </>
   );
 }
