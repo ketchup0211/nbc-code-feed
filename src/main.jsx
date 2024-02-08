d0feb607e9150750d80607bcc64db12695fee32b
leejieun2023 <lge9380@naver.com>
2024-02-09 00:21:09 +0900

    마이페이지

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom/client";
 import "./reset.css";
-import MyPage from "./pages/MyPage";
+//import MyPage from "./pages/MyPage";
+import Detail from "./pages/Detail";
 
 
-ReactDOM.createRoot(document.getElementById("root")).render(<MyPage />);
+ReactDOM.createRoot(document.getElementById("root")).render(<Detail />);
