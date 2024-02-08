46f310d984db096bdc3f0a4ec1eba39d2c89c81d
ketchup0211 <dlcka1111@naver.com>
2024-02-08 23:54:02 +0900

    Merged

--- src/redux/config/configstore.js
+++ src/redux/config/configstore.js
@@ -1,9 +1,7 @@
 import { combineReducers, createStore } from "redux";
 import SignUpReducer from "../modules/SignUpReducer";
-import users from "../modules/user";
-import postList from "../modules/postList";
 
-const RootReducer = combineReducers({ SignUpReducer, postList, users });
+const RootReducer = combineReducers({ SignUpReducer: SignUpReducer });
 
 const store = createStore(RootReducer);
 export default store;
