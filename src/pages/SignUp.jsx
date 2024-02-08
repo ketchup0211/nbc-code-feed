67b4643e4963f3290ee7a4295d7bc1a60a08e5c5
ketchup0211 <dlcka1111@naver.com>
2024-02-08 13:15:27 +0900

    [MOD] SignUp.jsx -> email sign up UI

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,3 +1,4 @@
+import { env } from "process";
 import { useState, useSelector, useDispatch, useContext } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
@@ -57,6 +58,7 @@ const HrDivider = styled.hr`
 
 const Button = styled.button`
   margin-bottom: 40px;
+  cursor: pointer;
 `;
 
 const FontSmall = styled.p`
@@ -70,23 +72,153 @@ const AuthLink = styled.p`
   margin-top: 20px;
   color: #3d3d4e;
 `;
+const BackButton = styled.button`
+  position: absolute;
+  top: 40px;
+  background-color: white;
+  border: 1.5px solid #e7e7e9;
+  margin: 20px 0px;
+  height: 40px;
+  width: 40px;
+  border-radius: 50%;
+  cursor: pointer;
+`;
+
+const AuthForm = styled.div``;
+const SignUpForm = styled.form``;
+const FormFieldGroup = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+const FormField = styled.div`
+  text-align: left;
+`;
+const FieldSet = styled.fieldset`
+  margin-bottom: 12px;
+`;
+const Label = styled.label`
+  display: block;
+  justify-content: space-between;
+  margin: 14px 0px 4px 0px;
+  font-size: 15px;
+  font-weight: 700w;
+`;
+const SignUpInput = styled.input`
+  width: 100%;
+  margin-right: 8px;
+  padding: 18px 20px;
+  border: 1.5px solid #e7e7e9;
+  border-radius: 12px;
+`;
+const CheckBox = styled.input`
+  margin: 0px 12px 0px 0px;
+  border: 1px solid #e7e7e9;
+  border-radius: 0px;
+  transform: scale(1.05);
+`;
 function SignUp() {
+  const [signUp, setSignUp] = useState(false);
+  const [name, setName] = useState("");
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleEmailSignUp = (event) => {
+    event.preventDefault();
+    switch (event.target.name) {
+      case "github":
+        //현재 페이지를 GitHub 로그인 페이지로 이동 (target=_self)
+        break;
+      case "email":
+        setSignUp(true);
+        break;
+      default:
+        console.log("유효하지 않은 접근입니다!");
+        return;
+    }
+  };
+  const handleDataBack = (event) => {
+    event.preventDefault();
+    setSignUp(false);
+  };
   return (
     <MainContainer>
       <AuthSidebar />
       <Content>
+        {signUp ? (
+          <BackButton type="button" onClick={handleDataBack}>
+            &lt;
+          </BackButton>
+        ) : null}
         <AuthContent>
           <SubTitle>Sign up to Code Feed</SubTitle>
-          <button>Sign up with GitHub</button>
-          <HrDivider></HrDivider>
-          <Button>Continue with email</Button>
-          <FontSmall>
-            By creating account you agree with our Terms of Sevice, Privacy
-            Policy, and our default Notification Settings.
-          </FontSmall>
-          <AuthLink>
-            Already have an account? <Link to="/login">Sign In</Link>
-          </AuthLink>
+          {signUp ? (
+            <AuthForm>
+              <SignUpForm>
+                <FormFieldGroup>
+                  <FormField>
+                    <FieldSet>
+                      <Label for="name">Name</Label>
+                      <SignUpInput id="name" />
+                    </FieldSet>
+                  </FormField>
+                  <FormField>
+                    <FieldSet>
+                      <FieldSet>
+                        <Label for="username">Username</Label>
+                        <SignUpInput id="username" />
+                      </FieldSet>
+                    </FieldSet>
+                  </FormField>
+                </FormFieldGroup>
+                <FormField>
+                  <FieldSet>
+                    <Label for="email">Email</Label>
+                    <SignUpInput id="email" />
+                  </FieldSet>
+                </FormField>
+                <FormField>
+                  <FieldSet>
+                    <Label for="password">Password</Label>
+                    <SignUpInput
+                      id="password"
+                      type="password"
+                      placeholder="6+characters"
+                    />
+                  </FieldSet>
+                </FormField>
+                <FormField>
+                  <FieldSet
+                    style={{
+                      display: "flex",
+                      marginTop: "30px",
+                    }}
+                  >
+                    <CheckBox id="user-agree" type="checkbox" />
+                    <Label for="user-agree" style={{ margin: "0px" }}>
+                      I agree with Code Feed's
+                    </Label>
+                  </FieldSet>
+                </FormField>
+              </SignUpForm>
+            </AuthForm>
+          ) : (
+            <>
+              <button type="button" name="github" style={{ cursor: "pointer" }}>
+                Sign up with GitHub
+              </button>
+              <HrDivider></HrDivider>
+              <Button type="button" name="email" onClick={handleEmailSignUp}>
+                Continue with email
+              </Button>
+              <FontSmall>
+                By creating account you agree with our Terms of Sevice, Privacy
+                Policy, and our default Notification Settings.
+              </FontSmall>
+              <AuthLink>
+                Already have an account? <Link to="/login">Sign In</Link>
+              </AuthLink>
+            </>
+          )}
         </AuthContent>
       </Content>
     </MainContainer>
