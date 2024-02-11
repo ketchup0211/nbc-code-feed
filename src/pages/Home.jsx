f0758aa61bed9c8563b5c1038d4c7d2b29c78444
DecidedCard <heajun35@gmail.com>
2024-02-11 19:30:46 +0900

    test: 파일업로드 코드 삭제

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,49 +1,20 @@
-import { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import FilterCheck from "src/components/HomeComponents/FilterCheck";
 import HomeHeader from "src/components/HomeComponents/HomeHeader";
 import PostList from "src/components/HomeComponents/PostList";
-import { db } from "src/firebase";
-import { addDoc, collection } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
 import FileUpload from "src/components/HomeComponents/test/FileUpload";
 import styled from "styled-components";
-import { addPost } from "src/redux/modules/postList";
+import { LinkStyle } from "src/util/Style";
 
 function Home() {
-  const { post } = useSelector((state) => state.postList);
-  const [content, setContent] = useState("");
   const navigate = useNavigate();
-  const dispatch = useDispatch();
-
-  const onchangeHandler = () => {
-    setContent(event.target.value);
-  };
-
-  const addTodo = async (event) => {
-    event.preventDefault();
-    const newTodo = { text: content, isDone: false };
-    dispatch(addPost([{ ...newTodo, id: crypto.randomUUID() }, ...post]));
-    setContent("");
-    const collectionRef = collection(db, "posts");
-    await addDoc(collectionRef, newTodo);
-  };
 
   return (
     <>
       <HomeHeader />
       <FilterCheck />
-      <PostList post={post} />
+      <PostList />
       <TestDiv>
-        <form onSubmit={addTodo}>
-          <input
-            type="text"
-            placeholder="내용을 입력해주세요"
-            value={content}
-            onChange={onchangeHandler}
-          />
-          <button>등록</button>
-        </form>
         <button onClick={() => navigate("/detail")}>
           디테일 페이지로 이동
         </button>
@@ -53,6 +24,7 @@ function Home() {
         <button onClick={() => navigate("/loginPage")}>로그인 페이지</button>
         <button onClick={() => navigate("/mypage")}>마이페이지</button>
         <button onClick={() => navigate("/")}>전체게시글</button>
+        <LinkStyle to={"/editProfile"}>프로필 수정 페이지</LinkStyle>
         <FileUpload />
       </TestDiv>
     </>
