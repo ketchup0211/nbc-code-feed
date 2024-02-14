4eacf737920448ddf126c9e74ae8ab81c33d7484
ketchup0211 <dlcka1111@naver.com>
2024-02-14 14:23:48 +0900

    add

--- src/redux/modules/postImageid.js
+++ src/redux/modules/postImageid.js
@@ -1,6 +1,5 @@
-const initialState = {
-  randomId: crypto.randomUUID(),
-};
+import { v4 as uuidv4 } from "uuid";
+const initialState = uuidv4();
 const postImageid = (state = initialState, action) => {
   switch (action.type) {
     default:
