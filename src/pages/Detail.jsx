b332e98aa37c3e6f14edc6a40ab1afa1f811c780
KeumSangHo <keumm9612@naver.com>
2024-02-14 11:39:21 +0900

    "merge"

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,41 +1,32 @@
 import DOMPurify from "dompurify";
 import HomeHeader from "src/components/HomeComponents/HomeHeader";
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import styled from "styled-components";
 import { collection, getDocs, query } from "firebase/firestore";
 //import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { db } from "src/firebase";
-
 function Detail() {
-  const navigate = useNavigate();
-
   const [contents, setContents] = useState([]);
   //const [explanation, setExplanation] = useState("");
   //const [selectedFile, setSelectedFile] = useState(null);
   //  const [edit, setEdit] = useState(true);
   const { id } = useParams();
-
   useEffect(() => {
     const fetchData = async () => {
       const q = query(collection(db, "posts"));
       const querySnapshot = await getDocs(q);
-
       const initialTodos = [];
       querySnapshot.forEach((doc) => {
         initialTodos.push({ id: doc.id, ...doc.data() });
       });
-
       setContents(initialTodos);
     };
-
     fetchData();
   }, []);
   const selectedData = contents.find((item) => item.id === id);
-
   // 'file' comes from the Blob or File API
   const sanitizer = DOMPurify.sanitize;
-
   if (!selectedData) {
     return;
   }
@@ -43,86 +34,26 @@ function Detail() {
   return (
     <UpperContainer>
       <HomeHeader />
-
       <Container key={selectedData.id}>
+        {/* <Fixed>
+          <AvatarImg src={wendy} />
+          <Fixedleft>{selectedData.name}</Fixedleft>
+        </Fixed> */}
         <Title>{selectedData.title}</Title>
-
         <WriteContainer
           dangerouslySetInnerHTML={{
             __html: sanitizer(selectedData.quillValue),
           }}
         ></WriteContainer>
       </Container>
-      {/*
-      <Container key={selectedData.id}>
-        <Title>{selectedData.title}</Title>
-        <Fixed>
-          <AvatarImg src={wendy} />
-          <Fixedleft>{selectedData.name}</Fixedleft>
-        </Fixed>
-        {edit ? (
-          <>
-            <img id="myimg" />
-            <ImageDiv
-              style={{ backgroundImage: `url(${selectedData.image})` }}
-            ></ImageDiv>
-            <Description>
-              {explanation === "" ? selectedData.content : explanation}
-            </Description>
-            <EditBtn
-              onClick={() => {
-                setEdit(false);
-              }}
-            >
-              수정하기
-            </EditBtn>
-          </>
-        ) : (
-          <>
-            <div>
-              <ImageDiv
-                style={{ backgroundImage: `url(${selectedData.image})` }}
-              >
-                <ImageEditBtn onClick={() => inputRef.current.click()}>
-                  {"X"}
-                </ImageEditBtn>
-              </ImageDiv>
-              <input
-                type="file"
-                accept="image/jpg, image/jpeg, image/png"
-                ref={inputRef}
-                style={{ display: "none" }}
-                onChange={handleFileChange}
-              />
-            </div>
-            <EditDescription
-              value={explanation == " " ? selectedData.content : explanation}
-              onChange={(e) => setExplanation(e.target.value)}
-            />
-            <EditBtn
-              onClick={() => {
-                handleEdit();
-                setEdit(true);
-              }}
-            >
-              수정완료
-            </EditBtn>
-          </>
-        )}
-      </Container>
-      */}
-
-      <button onClick={() => navigate("/WriteDetail")}>수정 페이지로</button>
+      {/* <button onClick={() => navigate("/WriteDetail")}>수정 페이지로</button> */}
     </UpperContainer>
   );
 }
-
 export default Detail;
-
 const UpperContainer = styled.div`
   overflow-x: hidden;
 `;
-
 const Container = styled.div`
   display: flex;
   width: 80%;
@@ -147,8 +78,7 @@ const WriteContainer = styled.div`
   padding: 30px;
   display: flex;
   flex-direction: column;
-  ql-align-center {
-    justify-content: center;
-    align-items: center;
+  .ql-align-center {
+    text-align: center;
   }
 `;
